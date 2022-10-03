386a166fc58b25cd203a177537b16d83e763f2ee
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-10-03 09:38:59 +0300

    Register search route

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,6 @@ Route::get('/productList',[ProductController::class,'productList']);
 Route::get('removecart/{id}',[ProductController::class,'removeCart']);
 Route::get('/ordernow',[ProductController::class,'orderNow']);
 Route::post('/orderplace',[ProductController::class,'orderPlace']);
+Route::get('/myorders',[ProductController::class,'myOrders']);
+Route::get('/search',[ProductController::class,'search']);
 
